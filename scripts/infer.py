282f077fc04ce5304442d98f64f8f401c80deb75
Lemuel PUGLISI <lpuglisi@comic2.cs.ucl.ac.uk>
2025-03-06 10:37:10 +0000

    update

--- scripts/infer.py
+++ scripts/infer.py
@@ -10,33 +10,27 @@ from tqdm import tqdm
 
 from wmh_synthseg.initialize import init_model
 from wmh_synthseg.consts import label_list_segmentation
-from wmh_synthseg.utils import (
-    MRIwrite, 
-    get_vram_usage, 
-    align_volume_to_ref
-)
+from wmh_synthseg.utils import MRIwrite, align_volume_to_ref
+
 
 @torch.inference_mode()
 def main():
 
-    parser = argparse.ArgumentParser(description="WMH-SynthSeg: joint segmentation of anatomy and white matter hyperintensities ", epilog='\n')
-    parser.add_argument("--i", help="Input image or directory.", required=True)
-    parser.add_argument("--o", help="Output segmentation (or directory, if the input is a directory)", required=True)
-    parser.add_argument("--model", help="Checkpoints path", required=True)
-    parser.add_argument("--device", default='cpu', help="device (cpu or cuda; optional)")
-    parser.add_argument("--threads", type=int, default=1, help="(optional) Number of CPU cores to be used. Default is 1. You can use -1 to use all available cores")
-    parser.add_argument("--save_lesion_probabilities", action="store_true", help="(optional) Saves lesion probability maps")
-    parser.add_argument("--crop", action="store_true", help="(optional) Does two passes, to limit size to 192x224x192 cuboid (needed for GPU processing)")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputs',     type=str, required=True,  help='text file where each line is the path of an image to process')
+    parser.add_argument('--outputs',    type=str, required=True,  help='text file where each line is the path to an output')
+    parser.add_argument("--model",      type=str, required=True,  help="model checkpoints")
+    parser.add_argument("--device",     type=str, default='cpu',  help="device (cpu or cuda; optional)")
+    parser.add_argument("--threads",    type=int, default=1,      help="(optional) Number of CPU cores to be used. Default is 1. You can use -1 to use all available cores")
     args = parser.parse_args()
 
-    input_path          = args.i
-    output_path         = args.o
-    device              = torch.device(args.device)
-    threads             = os.cpu_count() if args.threads < 0 else args.threads
-    save_lesion_probs   = args.save_lesion_probabilities
-    crop                = args.crop
-    model_path          = args.model
-    device              = torch.device(device)
+
+    inp_file    = args.inputs
+    out_file    = args.outputs
+    device      = torch.device(args.device)
+    threads     = os.cpu_count() if args.threads < 0 else args.threads
+    model_path  = args.model
+    device      = torch.device(device)
     
     print('Using ' + args.device)
     print('Using %s thread(s)' % threads)
@@ -46,52 +40,45 @@ def main():
     # run all the sanity checks
     #==================================
     
-    if not os.path.exists(input_path): raise Exception('Input does not exist')
-    if not os.path.exists(input_path): raise Exception('Model does not exist')
+    assert os.path.exists(inp_file), 'input file doesn\'t exist'
+    assert os.path.exists(out_file), 'output file doesn\'t exist'
 
-    if os.path.isfile(input_path):    
-        images_to_segment = [input_path]
-        segmentations_to_write = [output_path]
+    print('🚀 reading input files')
 
-    if os.path.isdir(input_path):  # directory
-        images_to_segment = []
-        segmentations_to_write = []
-        for im in os.listdir(input_path):
-            if im.endswith('.nii') or im.endswith('.nii.gz') or im.endswith('.mgz'):
-                images_to_segment.append(os.path.join(input_path, im))
-                segmentations_to_write.append(
-                    os.path.join(output_path, im.replace('.nii', '_seg.nii').replace('.mgz', '_seg.mgz')))
-        if len(images_to_segment) == 0:
-            raise Exception('Input directory does not contain images with supported type (.nii, .nii.gz, or .mgz)')
-        if output_path.endswith('.nii') or output_path.endswith('.nii.gz') or output_path.endswith('.mgz'):
-            raise Exception('If input is a directory, output should be a directory too')
-        if os.path.isdir(output_path) is False:
-            os.mkdir(output_path)
+    with open(inp_file, 'r') as f:
+        inp_list = [ l.strip() for l in f.readlines() ]
+
+    with open(out_file, 'r') as f:
+        out_list = [ l.strip() for l in f.readlines() ]
+    
+    for filepath, output_dir in zip(inp_list, out_list):        
+        
+        # check if the input file exists
+        if not os.path.exists(filepath):
+            raise Exception(f'{filepath} does not exist.')
+        
+        # Try to create the output directory
+        os.makedirs(output_dir, exist_ok=True)
     
     #==================================
-    # run all the sanity checks
+    # Load the model
     #==================================
-
-    model = init_model(model_path, device)
-    model.eval()
-    
     softmax = Softmax(dim=0)
+    model   = init_model(model_path, device)
+    model.eval()
     
     #========================================
     # params required in WMH-SynthSeg code.
     #========================================
-    label_list_segmentation_torch = torch.tensor(label_list_segmentation)
-    
-    n_neutral_labels    = 7
-    ref_res             = np.array([1.0, 1.0, 1.0])
-    voxelsize           = np.prod(ref_res)
     n_labels            = len(label_list_segmentation)
-
-    nlat  = int((n_labels - n_neutral_labels) / 2.0)
-
-    vflip = np.concatenate([np.array(range(n_neutral_labels)),
+    n_neutral_labels    = 7
+    label_list_segm     = torch.tensor(label_list_segmentation)
+    nlat                = int((n_labels - n_neutral_labels) / 2.0)
+    vflip               = np.concatenate([
+                            np.array(range(n_neutral_labels)),
                             np.array(range(n_neutral_labels + nlat, n_labels)),
-                            np.array(range(n_neutral_labels, n_neutral_labels + nlat))])
+                            np.array(range(n_neutral_labels, n_neutral_labels + nlat))
+                        ])
 
     #========================================
     # Loading and running.
@@ -110,10 +97,13 @@ def main():
         transforms.ScaleIntensity()
     ])
 
-    n_inputs = len(images_to_segment)
-    zipped   = zip(images_to_segment, segmentations_to_write)
+    n_inputs = len(inp_list)
+    zipped   = zip(inp_list, out_list)
 
-    for nim, (input_file, output_file) in tqdm(enumerate(zipped), total=n_inputs):
+    for nim, (input_file, output_dir) in tqdm(enumerate(zipped), total=n_inputs):
+        
+        output_segm_file = os.path.join(output_dir, 'wmh_segm.nii.gz')
+        output_prob_file = os.path.join(output_dir, 'wmh_segm_probs.nii.gz') 
         
         # assume the image is in MNI152 space
         affine_orig = nib.load(input_file).affine
@@ -150,24 +140,17 @@ def main():
             outp_orig_smax = softmax(outp_orig[0, 0:n_labels, ...])
             outp_flip_smax = softmax(outp_flip[0, vflip, ...])
             pred_seg_p = (.5 * outp_orig_smax + .5 * outp_flip_smax)
-            pred_seg = label_list_segmentation_torch[torch.argmax(pred_seg_p, 0)]
+            pred_seg = label_list_segm[torch.argmax(pred_seg_p, 0)]
             pred_seg = pred_seg.detach().cpu().numpy()
         
-        MRIwrite(pred_seg, affine_aligned, output_file)    
-                
-        if save_lesion_probs:
-            idx      = label_list_segmentation.index(77)
-            lesion_p = pred_seg_p[idx, ...]
-            lesion_p = np.squeeze(lesion_p.detach().cpu().numpy())
-            if output_file.endswith('.nii'):      name = output_file[:-4] + '.lesion_probs.nii'
-            elif output_file.endswith('.nii.gz'): name = output_file[:-7] + '.lesion_probs.nii.gz'
-            elif output_file.endswith('.mgz'):    name = output_file[:-4] + '.lesion_probs.mgz'
-            MRIwrite(lesion_p, affine_aligned, name)
-            
-        print('memory usage')
-        print(get_vram_usage())
-        print(f'{nim} done.')
+        # save wmh segmentation
+        MRIwrite(pred_seg, affine_aligned, output_segm_file)    
 
+        # save wmh probs                
+        idx      = label_list_segmentation.index(77)
+        lesion_p = pred_seg_p[idx, ...]
+        lesion_p = np.squeeze(lesion_p.detach().cpu().numpy())
+        MRIwrite(lesion_p, affine_aligned, output_prob_file)
 
 
 if __name__ == '__main__': main()
